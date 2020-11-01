6b4857c66c6a52457fd0e2d631869881ec246fdf
teplota <1729@inbox.ru>
2020-11-01 21:43:18 +0300

    Add file with request methods

---
+++ api_methods/request.py
@@ -0,0 +1,15 @@
+import requests
+import json
+from api_service.constants import HEADERS
+
+
+def create_user(api_url, params):
+    return requests.post(api_url, headers=HEADERS, data=json.dumps(params))
+
+
+def get_all_users(api_url):
+    return requests.get(api_url)
+
+
+def get_user_by_id(api_url, user_id):
+    return requests.get(f'{api_url}/{user_id}')
